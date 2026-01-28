6b42a44f63807edb085242da197750323793d3af
Sébastien VILLEBRUN <104035897+SVILLEBRUN@users.noreply.github.com>
2026-01-28 01:09:26 +0100

    Nuxt UI initial setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,13 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  css: ['./app/assets/css/main.css'],
-  vite: {
-    plugins: [
-      tailwindcss(),
-    ],
-  },
+    compatibilityDate: '2025-07-15',
+    devtools: { enabled: true },
+    css: ['./app/assets/css/main.css'],
+    vite: {
+        plugins: [
+            tailwindcss(),
+        ],
+    },
+    modules: ['@nuxt/ui']
 })
